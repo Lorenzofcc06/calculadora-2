87896831c39c5439705ea922db2292c057136387
Lorenzo Facco <lorenzofacco06@gmail.com>
2025-03-18 11:10:28 -0300

    Adicionando um histórico de operações na calculadora_flask

--- calculadora_flask/main_html_css.py
+++ calculadora_flask/main_html_css.py
@@ -1,13 +1,15 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 
 app = Flask(__name__)
+app.secret_key = "chave"
 
 def formatar_numero(numero):
     return int(numero) if numero.is_integer() else numero
 
 @app.route("/")
 def index():
-    return render_template("index.html") 
+    historico = session.get("historico",[])
+    return render_template("index.html", historico=historico) 
 
 @app.route("/calcular", methods=["POST"])
 def calcular():
@@ -16,27 +18,41 @@ def calcular():
         num2 = float(request.form["num2"])
         operacao = request.form["operacao"]
 
+        resultado = None
+        simbolo = ""
+
         if operacao == "soma":
             resultado = num1 + num2
+            simbolo = "+"
         
         elif operacao == "subtracao":
             resultado = num1 - num2
+            simbolo = "-"
 
         elif operacao == "multiplicacao":
             resultado = num1 * num2
+            simbolo = "×"
 
         elif operacao == "divisao":
             if num2 == 0:
-                return render_template("index.html", resultado=">>> ERRO <<< Divisão por zero!")
+                return render_template("index.html", resultado=">>> ERRO <<< Divisão por zero!", historico=session.get("historico",[]))
             resultado = num1 / num2
+            simbolo = "÷"
         else:
-            return render_template("index.html", resultado=">>> ERRO <<< Operação Inválida!") 
+            return render_template("index.html", resultado=">>> ERRO <<< Operação Inválida!",historico=session.get("historico",[])) 
 
         resultado_formatado = formatar_numero(resultado)       
-        return render_template("index.html", resultado=f"Resultado = {resultado_formatado}")
+
+        calculo = f"{formatar_numero(num1)} {simbolo} {formatar_numero(num2)} = {resultado_formatado}"
+
+        historico = session.get("historico",[])
+        historico.insert(0,calculo)
+        session["historico"] = historico[:5]
+
+        return render_template("index.html", resultado=f"Resultado = {resultado_formatado}", historico=historico)
     
     except ValueError:
-        return render_template("index.html", resultado=">>> ERRO <<< Digite valores númericos válidos!")
+        return render_template("index.html", resultado=">>> ERRO <<< Digite valores númericos válidos!", historico=session.get("historico",[]))
     
 if __name__ == "__main__":
     app.run(debug=True)
